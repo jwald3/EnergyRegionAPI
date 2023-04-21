11835a8a85d49eb8a062ef13e852d5bf48188297
Jordan <jwald3@brockport.edu>
2023-04-20 22:51:49 -0400

    test

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
-    dialect: "postgres",
-    logging: false,
-});
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL ||
+        "postgres://postgres:Tonka34*@localhost:5432/energyusage",
+    {
+        dialect: "postgres",
+        logging: false,
+    }
+);
 
 export default sequelize;
