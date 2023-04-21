6cbef3e3b9c335f43d33d365ce0c424ab9c1c936
Jordan <jwald3@brockport.edu>
2023-04-20 23:36:23 -0400

    updated SSL

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || "", {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
-    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
+    },
 });
 
 sequelize
