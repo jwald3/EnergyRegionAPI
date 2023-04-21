cd1788a0840efc21fc26c0596bc717a8dd23581d
Jordan <jwald3@brockport.edu>
2023-04-21 00:11:55 -0400

    updated app settings

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import express from "express";
-import regionRoutes from "./routes/regionRoutes.js";
+import regionRoutes from "./routes/regionRoutes.js"; // Adjust the import path if needed
 
 const app = express();
 
+// Middleware for parsing JSON and urlencoded data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Mount the region routes on the app
 app.use("/regions", regionRoutes);
 
-const port = process.env.PORT || 3000;
-
-export { app, port };
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
