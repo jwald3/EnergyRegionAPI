4952134bfd5fb9438962bb226bf72f6e01db7832
Jordan <jwald3@brockport.edu>
2023-04-21 07:54:14 -0400

    edited routes

--- src/routes/regionRoutes.js
+++ src/routes/regionRoutes.js
@@ -10,9 +10,9 @@ import {
 const router = Router();
 
 router.get("/", getAllRegions);
-router.get("/:region_id", getRegionById);
+router.get("/:RegionID", getRegionById);
 router.post("/", createRegion);
-router.put("/:region_id", updateRegion);
-router.delete("/:region_id", deleteRegion);
+router.put("/:RegionID", updateRegion);
+router.delete("/:RegionID", deleteRegion);
 
 export default router;
